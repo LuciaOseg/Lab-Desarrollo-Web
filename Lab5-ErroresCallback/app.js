e19625a38e6da41d6d3f292478682a4bdfbe7124
Lucia Oseguera <48133614+LuciaOseg@users.noreply.github.com>
2019-10-14 01:18:00 -0500

    Update app.js

--- Lab5-ErroresCallback/app.js
+++ Lab5-ErroresCallback/app.js
@@ -1,31 +1,83 @@
-
 const credentials = require('./credentials');
 const request = require('request');
 
 let city = "Sabinas Coahuila";
 
 //Obtiene los datos del clima Daily
-function getWeather(lat,long) {
+function getWeather(lat,long, callback) {
   request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`,
     function (error, response, body){
-      let jsonRequest = JSON.parse(body);
-      console.log(`${jsonRequest.daily.data[0].summary} Actualmente está a ${jsonRequest.currently.temperature}° C. Hay ${jsonRequest.daily.data[0].precipProbability * 100}% de posibilidad de lluvia.`);
+      //Errores
+      if(error){
+        callback("Servicio no disponible", undefined)
+      } else
+      if(response.body == 'Forbidden\n'){
+        callback('Las credenciales son incorrectas',undefined)
+      } else
+      if(response.body.code == 400){
+          callback(response.body.error, undefined);
+      } else
+      if(response.body == 'Not Found\n'){
+        callback('Lo sentimos el lugar no fue encontrado',undefined)
+      }
+      //Sin error
+      else {
+        let jsonRequest = JSON.parse(body);
+        let weatherData = {
+          summary: jsonRequest.daily.data[0].summary,
+          temperature: jsonRequest.currently.temperature,
+          precipProbability : jsonRequest.daily.data[0].precipProbability
+        }
+        callback(undefined,weatherData);
+      }
     }
   );
 }
 
 //Obtiene las coordenadas geograficas de la ciudad
-function getGeo(city_name) {
+function getGeo(city_name, callback) {
   request.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${credentials.MAPBOX_TOKEN}`,
-  function (error, response, body){
-    let jsonBody = JSON.parse(body);
-    let long = jsonBody.features[0].center[0];
-    let lat = jsonBody.features[0].center[1];
+    function (error, response, body){
+      let jsonBody = JSON.parse(body);
+      //Errores
+      if (error){
+        callback("Servicio no disponible",undefined)
+      } else
+      if(jsonBody.message == "Not Found"){
+        callback("No encontrado", undefined);
+      } else
+      if(jsonBody.message == 'Not Authorized - Invalid Token'){
+        callback("El token es invalido", undefined);
+      } else
+      if(jsonBody.features.length == 0){
+            callback("Lo sentimos el lugar no fue encontrado", undefined)
+      }
+      //Sin errores
+      else{
+        let coordinates = {
+          long: jsonBody.features[0].center[0],
+          lat: jsonBody.features[0].center[1]
+        }
+        callback(undefined, coordinates)
+      }
+    });
+  }
 
-    getWeather(lat, long)
-  });
-}
 
+  console.log(`${city.toUpperCase()}`);
 
-console.log(`${city.toUpperCase()}`);
-getGeo(city);
+  getGeo(city, function(error,response){
+    if(error){
+      console.log(error);
+    }
+    else{
+    //Una vez que se tienen las coordenadas, podemos obtener los datos climaticos
+    getWeather(response.lat, response.long, function(error, response){
+     if(error){
+       console.log(error);
+     }else{
+       console.log(`${response.summary} Actualmente está a ${response.temperature}° C. Hay ${response.precipProbability * 100}% de posibilidad de lluvia.`);
+     }
+   });
+ }
+});
